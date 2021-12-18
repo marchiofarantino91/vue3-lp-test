71fa32edc37acbe0d3959105c901d8979b594965
marchiofarantino91 <marchiofarantino91@gmail.com>
2021-12-19 01:10:52 +0700

    1 file modified

--- src/views/tools/calc.vue
+++ src/views/tools/calc.vue
@@ -4,19 +4,81 @@
   <div class="container">
     <div class="row">
       <div class="input-group mb-3">
-        <input
-          type="text"
-          class="form-control"
-          placeholder="Recipient's username"
-          aria-label="Recipient's username"
-          aria-describedby="basic-addon2"
-        />
-        <span class="input-group-text" id="basic-addon2">@example.com</span>
+        <input v-model="displayCalc" class="form-control" disabled />
+      </div>
+      <div class="row justify-content-center">
+        <div class="row justify-content-center mb-3">
+          <div class="col-1">
+            <button @click="addDisplay($event)" type="button" class="btn btn-outline-primary" id="bagi">/</button>
+          </div>
+          <div class="col-1">
+            <button @click="addDisplay($event)" type="button" class="btn btn-outline-primary" id="tambah">+</button>
+          </div>
+          <div class="col-1">
+            <button @click="addDisplay($event)" type="button" class="btn btn-outline-primary" id="kali">*</button>
+          </div>
+          <div class="col-1">
+            <button @click="addDisplay($event)" type="button" class="btn btn-outline-primary" id="kurang">-</button>
+          </div>
+        </div>
+        <div class="col-1">
+          <button @click="addDisplay($event)" type="button" class="btn btn-outline-primary" id="7">7</button>
+          <button @click="addDisplay($event)" type="button" class="btn btn-outline-primary" id="4">4</button>
+          <button @click="addDisplay($event)" type="button" class="btn btn-outline-primary" id="1">1</button>
+        </div>
+        <div class="col-1">
+          <button @click="addDisplay($event)" type="button" class="btn btn-outline-primary" id="8">8</button>
+          <button @click="addDisplay($event)" type="button" class="btn btn-outline-primary" id="5">5</button>
+          <button @click="addDisplay($event)" type="button" class="btn btn-outline-primary" id="2">2</button>
+        </div>
+        <div class="col-1">
+          <button @click="addDisplay($event)" type="button" id="9" class="btn btn-outline-primary">9</button>
+          <button @click="addDisplay($event)" type="button" id="4" class="btn btn-outline-primary">6</button>
+          <button @click="addDisplay($event)" type="button" id="3" class="btn btn-outline-primary">3</button>
+        </div>
+        <div class="col-1">
+          <div>
+            <button @click="addDisplay($event)" type="button" id="tambah" class="btn btn-outline-primary py-4">
+              +
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-5">
+          <button
+            @click="addDisplay($event)"
+            type="button"
+            class="btn btn-outline-primary"
+            style="padding: 5px 2.5rem 5px 2rem; margin-left: 1.2rem"
+            id="0"
+          >
+            0
+          </button>
+          <button @click="addDisplay($event)" type="button" id="enter" class="btn btn-outline-primary">=</button>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'Calculator',
+  data() {
+    return {
+      displayCalc: '',
+    }
+  },
+  methods: {
+    addDisplay(event) {
+      console.log(event.currentTarget.id)
+      let angka = ['1','2','3','4','5','6','7','8','9','0']
+      let selected = event.currentTarget.id;
+      
+      this.displayCalc += event.currentTarget.id;
+    },
+  },
+  computed: {},
+}
 </script>
