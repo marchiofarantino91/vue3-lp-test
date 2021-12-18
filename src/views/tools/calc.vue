4cc3624839dac28131d05e2459b9bac2d14c14b1
marchiofarantino91 <marchiofarantino91@gmail.com>
2021-12-19 00:25:44 +0700

    1 file modified

--- src/views/tools/calc.vue
+++ src/views/tools/calc.vue
@@ -1,6 +1,20 @@
 <template>
   <h1>Calculator</h1>
-    <router-link class="text-dark nav-link" to="/tools" >Previous</router-link>
+  <router-link class="text-dark nav-link" to="/tools">Previous</router-link>
+  <div class="container">
+    <div class="row">
+      <div class="input-group mb-3">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Recipient's username"
+          aria-label="Recipient's username"
+          aria-describedby="basic-addon2"
+        />
+        <span class="input-group-text" id="basic-addon2">@example.com</span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
